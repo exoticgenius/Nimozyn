85cf2c52ec4c837fb90b7c292a56966b23f04864
farshad sadeghi <Exoticgenius@outlook.com>
2025-06-29 18:24:30 +0330

    dev

--- Nimozyn/INimHandler.cs
+++ Nimozyn/INimHandler.cs
@@ -30,7 +30,7 @@ public static partial class NimDiscovery
             if (service.ImplementationType is null)
                 continue;
 
-            Type? extended = null;//Extend(service.ImplementationType, service.Lifetime);
+            Type? extended = Extend(service.ImplementationType, service.Lifetime);
 
             if (extended is null) continue;
 
@@ -53,6 +53,11 @@ public static partial class NimDiscovery
     private const string TYPE_PREFIX = "Runtime_Nim_Type_";
     private const string SUPERVISOR_FIELD_NAME = "Runtime_Nim_Supervisor_Field";
     private static readonly Type SUPERVISOR_TYPE = typeof(INimSupervisor);
+
+    static NimDiscovery()
+    {
+        ModuleBuilders = new();
+    }
 
     private static Type SelectSupervisor(ServiceLifetime lifetime) => lifetime switch
     {
@@ -80,10 +85,67 @@ public static partial class NimDiscovery
     {
         var descriptor = new NimServiceDescriptor { };
 
+        // input filter,
+        // pre
+        // on error
+        // post
+        // output filter
+        var aspect_matrix = new
+        {
+            asm_matrix = new
+            {
+                asm_input_filter = item.DeclaringType.Assembly.GetCustomAttributes()
+                    .Where(x => x is ANimAspect ana && ana.AspectPosition is AspectPosition.Pre && ana.BlockType is INimTransparentBlock).Select(x => x as ANimAspect).ToList(),
 
+                asm_pre = item.DeclaringType.Assembly.GetCustomAttributes()
+                    .Where(x => x is ANimAspect ana && ana.AspectPosition is AspectPosition.Pre && ana.BlockType is INimBlock).Select(x => x as ANimAspect).ToList(),
 
+                asm_on_error = item.DeclaringType.Assembly.GetCustomAttributes()
+                    .Where(x => x is ANimAspect ana && ana.BlockType is INimErrorBlock).Select(x => x as ANimAspect).ToList(),
 
+                asm_post = item.DeclaringType.Assembly.GetCustomAttributes()
+                    .Where(x => x is ANimAspect ana && ana.AspectPosition is AspectPosition.Post && ana.BlockType is INimBlock).Select(x => x as ANimAspect).ToList(),
 
+                asm_output_filter = item.DeclaringType.Assembly.GetCustomAttributes()
+                    .Where(x => x is ANimAspect ana && ana.AspectPosition is AspectPosition.Post && ana.BlockType is INimTransparentBlock).Select(x => x as ANimAspect).ToList()
+            },
+
+            type_matrix = new
+            {
+                asm_input_filter = item.DeclaringType.GetCustomAttributes()
+                    .Where(x => x is ANimAspect ana && ana.AspectPosition is AspectPosition.Pre && ana.BlockType is INimTransparentBlock).Select(x => x as ANimAspect).ToList(),
+
+                asm_pre = item.DeclaringType.GetCustomAttributes()
+                    .Where(x => x is ANimAspect ana && ana.AspectPosition is AspectPosition.Pre && ana.BlockType is INimBlock).Select(x => x as ANimAspect).ToList(),
+
+                asm_on_error = item.DeclaringType.GetCustomAttributes()
+                    .Where(x => x is ANimAspect ana && ana.BlockType is INimErrorBlock).Select(x => x as ANimAspect).ToList(),
+
+                asm_post = item.DeclaringType.GetCustomAttributes()
+                    .Where(x => x is ANimAspect ana && ana.AspectPosition is AspectPosition.Post && ana.BlockType is INimBlock).Select(x => x as ANimAspect).ToList(),
+
+                asm_output_filter = item.DeclaringType.GetCustomAttributes()
+                    .Where(x => x is ANimAspect ana && ana.AspectPosition is AspectPosition.Post && ana.BlockType is INimTransparentBlock).Select(x => x as ANimAspect).ToList()
+            },
+
+            method_matrix = new
+            {
+                asm_input_filter = item.GetCustomAttributes()
+                    .Where(x => x is ANimAspect ana && ana.AspectPosition is AspectPosition.Pre && ana.BlockType is INimTransparentBlock).Select(x => x as ANimAspect).ToList(),
+
+                asm_pre = item.GetCustomAttributes()
+                    .Where(x => x is ANimAspect ana && ana.AspectPosition is AspectPosition.Pre && ana.BlockType is INimBlock).Select(x => x as ANimAspect).ToList(),
+
+                asm_on_error = item.GetCustomAttributes()
+                    .Where(x => x is ANimAspect ana && ana.BlockType is INimErrorBlock).Select(x => x as ANimAspect).ToList(),
+
+                asm_post = item.GetCustomAttributes()
+                    .Where(x => x is ANimAspect ana && ana.AspectPosition is AspectPosition.Post && ana.BlockType is INimBlock).Select(x => x as ANimAspect).ToList(),
+
+                asm_output_filter = item.GetCustomAttributes()
+                    .Where(x => x is ANimAspect ana && ana.AspectPosition is AspectPosition.Post && ana.BlockType is INimTransparentBlock).Select(x => x as ANimAspect).ToList()
+            },
+        };
 
 
 
@@ -173,55 +235,55 @@ public static partial class NimDiscovery
         il.DeclareLocal(methodinfo.ReturnType); // 0
         il.DeclareLocal(typeof(Exception)); //     1
 
-        if (!IsTask(returnType))
-        {
-            var exBlock = il.BeginExceptionBlock();
-        }
+        if (IsTask(returnType))
+            return;
+
+
+
+        var exBlock = il.BeginExceptionBlock();
 
         for (var i = 0; i < parameters.Count + 1; i++)
             il.Emit(OpCodes.Ldarg, i);
         il.Emit(OpCodes.Call, methodinfo);
 
-        if (!IsTask(returnType))
+
+
+
+
+
+
+
+
         {
+            //else if (typeof(Task<>).IsAssignableFrom(returnType))
+            //{
+            //    Type[] genArg = [returnType.GenericTypeArguments[0]];
 
+            //    var suppressor = "SuppressTask";
+
+            //    il.Emit(OpCodes.Call, typeof(NimDiscovery).GetMethods().First(x => x.Name == suppressor).MakeGenericMethod(genArg));
+            //}
+            //else if (typeof(ValueTask<>).IsAssignableFrom(returnType))
+            //{
+            //    Type[] genArg = [returnType.GenericTypeArguments[0]];
+
+            //    var suppressor = "SuppressValueTask";
+
+            //    il.Emit(OpCodes.Call, typeof(NimDiscovery).GetMethods().First(x => x.Name == "SuppressValueTask").MakeGenericMethod(genArg));
+            //}
+            //else if (typeof(Task).IsAssignableFrom(returnType))
+            //{
+            //    var suppressor = "SuppressTaskVoid";
+
+            //    il.Emit(OpCodes.Call, typeof(NimDiscovery).GetMethods().First(x => x.Name == suppressor).MakeGenericMethod([]));
+            //}
+            //else if (typeof(ValueTask).IsAssignableFrom(returnType))
+            //{
+            //    var suppressor = "SuppressValueTaskVoid";
+
+            //    il.Emit(OpCodes.Call, typeof(NimDiscovery).GetMethods().First(x => x.Name == "SuppressValueTask").MakeGenericMethod([]));
+            //}
         }
-        else if (typeof(Task<>).IsAssignableFrom(returnType))
-        {
-            Type[] genArg = [returnType.GenericTypeArguments[0]];
-
-            var suppressor = "SuppressTask";
-
-            il.Emit(OpCodes.Call, typeof(NimDiscovery).GetMethods().First(x => x.Name == suppressor).MakeGenericMethod(genArg));
-        }
-        else if (typeof(ValueTask<>).IsAssignableFrom(returnType))
-        {
-            Type[] genArg = [returnType.GenericTypeArguments[0]];
-
-            var suppressor = "SuppressValueTask";
-
-            il.Emit(OpCodes.Call, typeof(NimDiscovery).GetMethods().First(x => x.Name == "SuppressValueTask").MakeGenericMethod(genArg));
-        }
-        else if (typeof(Task).IsAssignableFrom(returnType))
-        {
-            var suppressor = "SuppressTaskVoid";
-
-            il.Emit(OpCodes.Call, typeof(NimDiscovery).GetMethods().First(x => x.Name == suppressor).MakeGenericMethod([]));
-        }
-        else if (typeof(ValueTask).IsAssignableFrom(returnType))
-        {
-            var suppressor = "SuppressValueTaskVoid";
-
-            il.Emit(OpCodes.Call, typeof(NimDiscovery).GetMethods().First(x => x.Name == "SuppressValueTask").MakeGenericMethod([]));
-        }
-
-
-
-
-
-
-
-
     }
 
     [MethodImpl(MethodImplOptions.Synchronized)]
@@ -285,14 +347,13 @@ public static partial class NimDiscovery
     }
 
 }
-
 public class NimServiceDescriptor
 {
-    public List<INimBlock> InputFilterBlocks { get; set; }
+    public List<INimTransparentBlock> InputFilterBlocks { get; set; }
     public List<INimBlock> PreExecuteBlocks { get; set; }
+    public List<INimErrorBlock> OnErrorBlocks { get; set; }
     public List<INimBlock> PostExecuteBlocks { get; set; }
-    public List<INimBlock> OnErrorBlocks { get; set; }
-    public List<INimBlock> OutputFilterBlocks { get; set; }
+    public List<INimTransparentBlock> OutputFilterBlocks { get; set; }
 }
 
 public interface INimBlock;
@@ -309,45 +370,66 @@ public class NimBlockTest : INimNeutralBlock
 }
 
 
+public interface INimErrorBlock : INimBlock;
+public interface INimErrorBlock<T> : INimErrorBlock where T : INimInput
+{
+    Task<T> Execute(T request, Exception e);
+}
+public class NimErrorBlockTest<T> : INimErrorBlock<T> where T : INimInput
+{
+    public Task<T> Execute(T request, Exception e)
+    {
+        throw new NotImplementedException();
+    }
+}
 
 public interface INimTransparentBlock : INimBlock;
 public interface INimTransparentBlock<T> : INimTransparentBlock where T : INimInput
 {
-    Task Execute(T request);
+    Task<T> Execute(T request);
 }
 public class NimTransparentBlockTest<T> : INimTransparentBlock<T> where T : INimInput
 {
-    public Task Execute(T request)
+    public Task<T> Execute(T request)
     {
         throw new NotImplementedException();
     }
 }
 
-
-
-public interface INimTransformBlock : INimBlock;
-public interface INimTransformBlock<T, R> : INimTransformBlock where T : INimInput where R : T
+public interface INimAspect;
+public abstract class ANimAspect : Attribute, INimAspect
 {
-    Task<R> Execute(T request);
-}
-public class NimTransformBlockTest<T, R> : INimTransformBlock<T, R> where T : INimInput where R : T
-{
-    public Task<R> Execute(T request)
+    public Type BlockType { get; private init; }
+    public ServiceLifetime TargetLifeTime { get; private init; }
+    public AspectPosition AspectPosition { get; private init; }
+
+    protected ANimAspect(Type blockType, ServiceLifetime targetLifeTime, AspectPosition position = AspectPosition.NotSpecified)
     {
-        throw new NotImplementedException();
+        BlockType = blockType;
+        TargetLifeTime = targetLifeTime;
+        AspectPosition = position;
     }
 }
 
-
-
-public class NimRequestLinkerAttribute<T>(ServiceLifetime targetLifeTime) : Attribute where T : INimInput
+public class NimRequestLinkerAttribute<T> : ANimAspect where T : INimInput
 {
-    public Type BlockType => typeof(T);
+    public NimRequestLinkerAttribute(ServiceLifetime targetLifeTime, AspectPosition position = AspectPosition.NotSpecified) : base(typeof(T), targetLifeTime, position)
+    {
+    }
 }
 
-public class NimAspectLinkerAttribute<T>(ServiceLifetime targetLifeTime) : Attribute where T : INimBlock
+public class NimAspectLinkerAttribute<T> : ANimAspect where T : INimBlock
 {
-    public Type BlockType => typeof(T);
+    public NimAspectLinkerAttribute(ServiceLifetime targetLifeTime, AspectPosition position = AspectPosition.NotSpecified) : base(typeof(T), targetLifeTime, position)
+    {
+    }
 }
 
 public class NimGeneratedClassAttribute : Attribute;
+
+public enum AspectPosition
+{
+    NotSpecified,
+    Pre,
+    Post,
+}
--- Test/Program.cs
+++ Test/Program.cs
@@ -2,6 +2,8 @@
 
 using Nimozyn;
 
+[assembly: NimAspectLinker<NimLogger1>(ServiceLifetime.Scoped)]
+
 var col = new ServiceCollection();
 
 col.AddTransient<ITestService, TestService>();
@@ -10,16 +12,16 @@ col.ScanNimHandlersAsync();
 
 var provider = col.BuildServiceProvider();
 
+Console.ReadLine();
 
 
-
-
-public interface ITestService
+public interface ITestService : INimHandler
 {
     int TestMethod1(TestRequest1 req);
     string TestMethod2(TestRequest2 req);
 }
 
+[NimAspectLinker<NimLogger1>(ServiceLifetime.Scoped, AspectPosition.Pre)]
 public class TestService : ITestService
 {
     public int TestMethod1(TestRequest1 req)
@@ -44,7 +46,10 @@ public class TestRequest2 : INimRequest<string>
 }
 
 [NimRequestLinker<TestRequest1>(ServiceLifetime.Scoped)]
-public class NimLogger1
+public class NimLogger1 : INimNeutralBlock
 {
-
+    public Task Execute()
+    {
+        throw new NotImplementedException();
+    }
 }
